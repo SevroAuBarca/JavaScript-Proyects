57e7c9ffaeaa7f29ca4f147933019e6b4b19df68
SevroAuBarca <57334088+SevroAuBarca@users.noreply.github.com>
2021-09-14 00:00:11 -0500

    Add files via upload

---
+++ 1.-Beginner/Lorem-ipsum-generator/precache-manifest.74f686b839e56cee9b34955eb4704ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033e839ac0f92bb015d3",
+    "url": "/css/app.d40fc157.css"
+  },
+  {
+    "revision": "b00e1167ce0beda20f1d3f31e808525e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "033e839ac0f92bb015d3",
+    "url": "/js/app.294ff9a7.js"
+  },
+  {
+    "revision": "0c005fd4fc36df23ed5b",
+    "url": "/js/chunk-vendors.2404e180.js"
+  },
+  {
+    "revision": "88b272a53587d65bba1b720c6b7485cd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
